9d4803203a069febc5f962f8182d368f80924b8c
Subhom <subhomghosh836@gmail.com>
2026-07-28 23:21:29 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child1'>Hello world</h1>
+      <h2 id='child2'>Subhom</h2>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
---
+++ Day1/app.js
@@ -0,0 +1,3 @@
+export const arr = [10,20,30]
+const user = 'Subhom'
+export default user
